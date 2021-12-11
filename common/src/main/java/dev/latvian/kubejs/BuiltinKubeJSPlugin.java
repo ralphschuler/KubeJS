75285ce3cc876f5752878de99f50c087ee7324f3
Ralph Schuler <48420064+ralphschuler@users.noreply.github.com>
2021-12-12 00:08:36 +0100

    Update BuiltinKubeJSPlugin.java

--- common/src/main/java/dev/latvian/kubejs/BuiltinKubeJSPlugin.java
+++ common/src/main/java/dev/latvian/kubejs/BuiltinKubeJSPlugin.java
@@ -185,29 +185,30 @@ public class BuiltinKubeJSPlugin extends KubeJSPlugin {
 		filter.allow("java.lang.CharSequence");
 
 		filter.allow("java.crypto");
+		filter.allow("java.crypto.KeyFactory");
 		filter.allow("java.security");
 		
 		filter.allow("java.math.BigInteger"); // java.math
 		filter.allow("java.math.BigDecimal");
 
-		filter.deny("java.io"); // IO
+		//filter.deny("java.io"); // IO
 		filter.allow("java.io.Closeable");
 		filter.allow("java.io.Serializable");
 
-		filter.deny("java.nio"); // NIO
+		//filter.deny("java.nio"); // NIO
 		filter.allow("java.nio.ByteOrder");
 
 		filter.allow("java.util"); // Utils
-		filter.deny("java.util.jar");
-		filter.deny("java.util.zip");
+		//filter.deny("java.util.jar");
+		//filter.deny("java.util.zip");
 
 		filter.allow("it.unimi.dsi.fastutil"); // FastUtil
 
 		filter.allow("dev.latvian.kubejs"); // KubeJS
-		filter.deny("dev.latvian.kubejs.script");
-		filter.deny("dev.latvian.kubejs.mixin");
-		filter.deny(KubeJSPlugin.class);
-		filter.deny(KubeJSPlugins.class);
+		//filter.deny("dev.latvian.kubejs.script");
+		//filter.deny("dev.latvian.kubejs.mixin");
+		//filter.deny(KubeJSPlugin.class);
+		//filter.deny(KubeJSPlugins.class);
 
 		filter.allow("net.minecraft"); // Minecraft
 		filter.allow("com.mojang.authlib.GameProfile");
@@ -217,14 +218,14 @@ public class BuiltinKubeJSPlugin extends KubeJSPlugin {
 		filter.allow("me.shedaniel.architectury"); // Architectury
 
 		// Misc
-		filter.allow("java.net"); // Networks
-		filter.deny("sun"); // Sun
-		filter.deny("com.sun"); // Sun
-		filter.deny("io.netty"); // Netty
-		filter.deny("org.objectweb.asm"); // ASM
-		filter.deny("org.spongepowered.asm"); // Sponge ASM
-		filter.deny("org.openjdk.nashorn"); // Nashorn
-		filter.deny("jdk.nashorn"); // Nashorn
+		//filter.allow("java.net"); // Networks
+		//filter.deny("sun"); // Sun
+		//filter.deny("com.sun"); // Sun
+		//filter.deny("io.netty"); // Netty
+		//filter.deny("org.objectweb.asm"); // ASM
+		//filter.deny("org.spongepowered.asm"); // Sponge ASM
+		//filter.deny("org.openjdk.nashorn"); // Nashorn
+		//filter.deny("jdk.nashorn"); // Nashorn
 
 		// Mods
 		filter.allow("mezz.jei"); // JEI
